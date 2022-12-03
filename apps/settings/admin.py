69b1e032cb20905a519af9d00f9d7d89113546c6
Nurbolottop <erk1nbaevw2711@gmail.com>
2022-12-04 01:02:40 +0600

    First

--- apps/settings/admin.py
+++ apps/settings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from apps.settings.models import Settings
+from apps.settings.models import Settings,Slide
 
 # Register your models here.
 
 admin.site.register(Settings)
+admin.site.register(Slide)
--- apps/settings/models.py
+++ apps/settings/models.py
@@ -25,9 +25,19 @@ class Settings(models.Model):
          
 
 class Slide(models.Model):
+
     slide_image_1 = models.ImageField(
         upload_to='slide_image/',
         verbose_name="Фотография первого слайда"
+    )
+
+    name_slide_1 = models.CharField(
+        max_length=244,
+        verbose_name="Название первого слайда"
+    )
+
+    desc_slide_1 = models.TextField(
+        verbose_name="Описание первого слайда"
     )
 
     slide_image_2 = models.ImageField(
@@ -35,5 +45,19 @@ class Slide(models.Model):
         verbose_name="Фотография второго слайда"
     )
 
-    
-    
+    name_slide_2 = models.CharField(
+        max_length=244,
+        verbose_name="Название второго слайда"
+    )
+
+    desc_slide_2 = models.TextField(
+        verbose_name="Описание первого слайда"
+    )
+
+    def __str__(self):
+        return self.name_slide_1
+
+    class Meta:
+        verbose_name = "Слайды"
+        verbose_name_plural = "Слайды"
+        
